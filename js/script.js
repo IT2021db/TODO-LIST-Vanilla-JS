7690115baaa74e7e038460f107127071b8ca3d38
Dorota Borowska <youcode2021db@gmail.com>
2021-05-29 19:14:55 +0200

    initialize commit

---
+++ js/script.js
@@ -0,0 +1,79 @@
+{
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        const newTaskElement = document.querySelector(".js-newTask");
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskElement.value = "";
+        }
+        newTaskElement.focus();
+    };
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+        <li class="tasks__item" >
+             <button class="tasks__button tasks__button--toggleDone js-done">
+         ${task.done ? "✓" : ""}
+         </button>
+         <span class="tasks__content
+         ${task.done ? "tasks__content--done" : ""}">
+         ${task.content} 
+        </span> 
+      
+        
+        <button class="tasks__button tasks__button--remove js-remove">
+        &#x1F5D1
+        </button>
+      
+        </li>
+        `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+        bindEvents();
+    };
+
+    const init = () => {
+        render();
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+    };
+    init();
+}
